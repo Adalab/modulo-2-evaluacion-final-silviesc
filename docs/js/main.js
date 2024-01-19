90a51d5ed9fa4c7d0deeeb8078df3b07c886919f
Silvia Escanciano <silvia.escanciano@gmail.com>
2024-01-19 11:58:55 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,8 +1,8 @@
-const a=document.querySelector(".js-input"),m=document.querySelector(".js-btnSubmit");document.querySelector(".js-btnReset");const l=document.querySelector(".js-results"),o=document.querySelector(".js-favs");let s=[],n=[];function u(e){return`<li>
-        <img src="${e.images.jpg.image_url}" alt="film photo" />
-        <h3>${e.title}</h3>
-    </li>`}function d(e){l.innerHTML="";for(let t=0;t<e.length;t++)l.innerHTML+=u(e[t])}function g(e){e.preventDefault();const t=a.value;fetch(`https://api.jikan.moe/v4/anime?q=${t}`).then(i=>i.json()).then(i=>{s=i.data,d(s)})}m.addEventListener("click",g);function r(e){o.innerHTML="";for(let t=0;t<e.length;t++){const i=document.createElement("li");i.innerHTML=`
-            <img src="${e[t].images.jpg.image_url}" alt="film photo" />
-            <h3>${e[t].title}</h3>
-        `,o.appendChild(i)}}const f=e=>{const t=e.target.parentNode;if(t){t.classList.toggle("fav");const i=s.find(c=>c.title===t.querySelector("h3").textContent);n=JSON.parse(localStorage.getItem("favoriteFilms")),n.push(i),r(n),localStorage.setItem("favoriteFilms",JSON.stringify(n))}};l.addEventListener("click",f);const p=JSON.parse(localStorage.getItem("favoriteFilms"));r(p);
+const c=document.querySelector(".js-input"),d=document.querySelector(".js-btnSubmit");document.querySelector(".js-btnReset");const s=document.querySelector(".js-results"),r=document.querySelector(".js-favs");let l=[];function u(t){return`<li>
+        <img src="${t.images.jpg.image_url}" alt="film photo" />
+        <h3>${t.title}</h3>
+    </li>`}function m(t){s.innerHTML="";for(let e=0;e<t.length;e++)s.innerHTML+=u(t[e])}function g(t){t.preventDefault();const e=c.value;fetch(`https://api.jikan.moe/v4/anime?q=${e}`).then(n=>n.json()).then(n=>{l=n.data,m(l)}).catch(n=>{console.error("Error reaching data:",n)})}d.addEventListener("click",g);function a(t){r.innerHTML="";for(let e=0;e<t.length;e++){const n=document.createElement("li");n.innerHTML=`
+            <img src="${t[e].images.jpg.image_url}" alt="film photo" />
+            <h3>${t[e].title}</h3>
+        `,r.appendChild(n)}}const f=t=>{const e=t.target.parentNode;if(e){e.classList.add("fav");const n=l.find(o=>o.title===e.querySelector("h3").textContent),i=JSON.parse(localStorage.getItem("favoriteFilms"))||[];i.findIndex(o=>o.title===n.title)===-1?(i.push(n),a(i),localStorage.setItem("favoriteFilms",JSON.stringify(i))):console.log(`${n.title} ya está en la lista de favoritos.`)}};s.addEventListener("click",f);const h=JSON.parse(localStorage.getItem("favoriteFilms"));a(h);
 //# sourceMappingURL=main.js.map
